5f941072cfe188211088230d4ec2e7c40a55199f
leahteich <leahteich@gmail.com>
2023-04-24 15:45:25 -0400

    push

--- betterhuds/app.py
+++ betterhuds/app.py
@@ -2,7 +2,7 @@
 # -- Import section --
 from flask import Flask
 from flask import render_template
-# from flask import request
+from flask import request
 from categories import categories_wanted, recipes, locations
 import requests
 from multiprocessing import Pool
@@ -12,14 +12,28 @@ app = Flask(__name__)
 
 
 # -- Routes section --
-@app.route("/")
-@app.route("/index")
+@app.route("/", methods=['GET','POST'])
+@app.route("/index", methods=['GET','POST'])
 def index():
-    response = requests.get("https://api.cs50.io/dining/menus", {"date": "2023-05-09", "location": 8, "meal": 1})
-    menu = response.json()
-    menu_items = []
-    for item in menu: 
-        recipe = recipes[item['recipe']]
-        recipe['category'] = item['category']
-        menu_items.append(recipe)
-    return render_template("index.html", menu=menu_items, locations=locations)
+    if request.method == 'GET':
+        response = requests.get("https://api.cs50.io/dining/menus", {"date": "2023-04-23", "location": 8, "meal": 1})
+        menu = response.json()
+        menu_items = []
+        for item in menu: 
+            recipe = recipes[item['recipe']]
+            recipe['category'] = item['category']
+            menu_items.append(recipe)
+        return render_template("index.html", menu=menu_items, locations=locations)
+    else:
+        my_dhall = request.form.get("dhalls")
+        my_date = request.form.get("datepick")
+        # print(my_dhall)
+        # print(locations[5])
+        response = requests.get("https://api.cs50.io/dining/menus", {"date": my_date, "location": my_dhall, "meal": 1})
+        menu = response.json()
+        menu_items = []
+        for item in menu: 
+            recipe = recipes[item['recipe']]
+            recipe['category'] = item['category']
+            menu_items.append(recipe)
+        return render_template("index.html", menu=menu_items, locations=locations)
