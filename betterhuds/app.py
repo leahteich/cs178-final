00d707a89552c8f77ebc49563f3946bf86d60f79
leahteich <leahteich@gmail.com>
2023-04-26 15:18:23 -0400

    changes

--- betterhuds/app.py
+++ betterhuds/app.py
@@ -16,24 +16,17 @@ app = Flask(__name__)
 @app.route("/index", methods=['GET','POST'])
 def index():
     if request.method == 'GET':
-        response = requests.get("https://api.cs50.io/dining/menus", {"date": "2023-04-23", "location": 8, "meal": 1})
-        menu = response.json()
-        menu_items = []
-        for item in menu: 
-            recipe = recipes[item['recipe']]
-            recipe['category'] = item['category']
-            menu_items.append(recipe)
-        return render_template("index.html", menu=menu_items, locations=locations)
-    else:
+        response = requests.get("https://api.cs50.io/dining/menus", {"date": "2023-04-25", "location": 8, "meal": 1})
+    else: 
         my_dhall = request.form.get("dhalls")
         my_date = request.form.get("datepick")
-        # print(my_dhall)
-        # print(locations[5])
+        print(my_dhall)
         response = requests.get("https://api.cs50.io/dining/menus", {"date": my_date, "location": my_dhall, "meal": 1})
-        menu = response.json()
-        menu_items = []
-        for item in menu: 
+    menu = response.json()
+    menu_items = []
+    for item in menu: 
+        if item['recipe'] in recipes: 
             recipe = recipes[item['recipe']]
-            recipe['category'] = item['category']
-            menu_items.append(recipe)
-        return render_template("index.html", menu=menu_items, locations=locations)
+        recipe['category'] = item['category']
+        menu_items.append(recipe)
+    return render_template("index.html", menu=menu_items, locations=locations)
