f3d71a3189eeea790a842c072f4aae08483dd4bd
Sofiya Lysenko <sofiya.sofie02@gmail.com>
2023-05-02 13:16:06 -0400

    Added comments

--- betterhuds/app.py
+++ betterhuds/app.py
@@ -110,6 +110,7 @@ def index():
                            locations=locations, loc=dining_hall, date=date, date_string=date_string)
 
 # Help from ChatGPT and TF Felix
+# Rate (state): upvoted, downvoted, unrated 
 @app.route('/api/upvote/<item_id>', methods=['POST'])
 def upvote(item_id):
     item_id = int(item_id)
